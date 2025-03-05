4af22a305658c37b1e912af44f5ed1665e64f0da
Ermand Haruni <88191097+ErmandH@users.noreply.github.com>
2025-03-05 11:30:57 +0300

    up w1

--- week1_2025_05_03.m
+++ week1_2025_05_03.m
@@ -1,4 +1,5 @@
-clc; clear;
+%% Uygulama
+clc; clear; close all;
 %B = [ 0 5 5 ; 70 8 7 ; 10 1 3]
 % save B katsayilar // değişkeni katsayilar.mat olarak kaydetmek için
 
@@ -10,3 +11,8 @@ triu_b = triu(B)
 tril_b = tril(B)
 
 C = [B zeros(3,2)]
+%% structure array
+person.name = "Ermand";
+person.surname = "Haruni";
+person.phone = "05394111175";
+disp(person)
